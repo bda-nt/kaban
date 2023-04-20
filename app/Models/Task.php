16f5aba117cd668bb6cc261d7d460ef704dc49ec
bda-nt <bav-nt@mail.ru>
2023-04-20 21:03:01 +0500

    Started fill Task.php

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -7,5 +7,42 @@ use Illuminate\Database\Eloquent\Model;
 
 class Task extends Model
 {
+    const TEAM = [
+        1 => 'Оценка',
+        2 => 'Канбан',
+        3 => 'Гант'
+    ];
+
+    const PROJECT = [
+        1 => 'Test Project #1',
+        2 => 'Test Project #2',
+        3 => 'Test Project #3',
+    ]
+
     use HasFactory;
+
+    protected $connection = 'kaban';
+    protected $table = 'tasks';
+    protected $guarded = ['id'];
+
+    /**
+     * TODO: CHANGE WHEN AUTH DATABASE CREATED
+     *
+     * @param int $id
+     * @return String
+     */
+    public function team(int $id) : String
+    {
+        return self::TEAM[$id];
+    }
+
+    public function project(int $id) : string
+    {
+        return self::PROJECT[$id];
+    }
+
+    public function parents(int $id)
+    {
+        return Task::all();
+    }
 }
