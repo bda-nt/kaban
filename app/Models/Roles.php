acb69cdc6db2bdc65d386ba24f30419a0522b922
bda-nt <bav-nt@mail.ru>
2023-04-20 20:44:37 +0500

    Fill Roles.php

--- app/Models/Roles.php
+++ app/Models/Roles.php
@@ -8,4 +8,21 @@ use Illuminate\Database\Eloquent\Model;
 class Roles extends Model
 {
     use HasFactory;
+
+    protected $connection = 'kaban';
+    protected $table = 'roles';
+    protected $guarded = ['id'];
+    protected $fillable = [
+        'name'
+    ];
+    public $timestamps = true;
+
+    /**
+     * TODO: ADD RELATIONSHIP TO EXECUTORS
+     * @return int
+     */
+    public function executors()
+    {
+        return 0;
+    }
 }
