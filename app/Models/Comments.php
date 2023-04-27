c6d58c9d169bf70bd0bf9447bccecec856dca18c
bda-nt <bav-nt@mail.ru>
2023-04-27 19:42:36 +0500

    Fill Comment.php

--- app/Models/Comments.php
+++ app/Models/Comments.php
@@ -4,8 +4,17 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Task;
 
 class Comments extends Model
 {
     use HasFactory;
+
+    protected $guarded = ['id'];
+    public $timestamps = true;
+
+    public function task()
+    {
+        return Task::all();
+    }
 }
