c1ccda2c786a1eeece327263a904e6f838718a9b
bda-nt <bav-nt@mail.ru>
2023-04-27 19:51:38 +0500

    Fill Executors.php

--- app/Models/Stages.php
+++ app/Models/Stages.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Stages extends Model
 {
     use HasFactory;
+
+    protected $guarded = ['id'];
+    public $timestamps = true;
+
+    public function task()
+    {
+        return Task::all();
+    }
 }
