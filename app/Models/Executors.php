faec1808b5bd77c07f1220465fa743b47ffa747a
bda-nt <bav-nt@mail.ru>
2023-05-17 11:47:43 +0500

    Add relationship

--- app/Models/Executors.php
+++ app/Models/Executors.php
@@ -5,6 +5,27 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * App\Models\Executors
+ *
+ * @property int $id
+ * @property int $task_id
+ * @property int $user_id
+ * @property int $role_id
+ * @property string|null $time_spent
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @method static \Illuminate\Database\Eloquent\Builder|Executors newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Executors newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Executors query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Executors whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Executors whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Executors whereRoleId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Executors whereTaskId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Executors whereTimeSpent($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Executors whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Executors whereUserId($value)
+ */
 class Executors extends Model
 {
     use HasFactory;
@@ -17,16 +38,17 @@ class Executors extends Model
 
     public function role()
     {
-        return Roles::all();
+        return $this->hasOne(Roles::class);
     }
 
     public function user()
     {
-        return 0;
+        return;
     }
 
     public function task()
     {
-        return Task::all();
+        return $this->hasOne(Task::class);
     }
 }
+21
